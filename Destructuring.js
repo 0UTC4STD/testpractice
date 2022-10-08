3a17fca69c7d1364849f529dd17a4c55b571ba3a
0UTC4STD <dylancourte@yahoo.com>
2022-10-08 16:38:13 -0400

    added maps and sets exer

--- Destructuring.js
+++ Destructuring.js
@@ -28,3 +28,44 @@ arr[1] = temp;
 raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
 
 const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
+
+
+
+// Maps and Sets Excerise
+
+new Set([1,1,2,2,3,4])
+// {1,2,3,4}
+
+[...new Set("referee")].join("")
+// ref
+
+let m = new Map();
+m.set([1,2,3], true);
+m.set([1,2,3], false);
+// 0: {Array(3) => true}, 1: {Array(3) => false}
+
+// hasDuplicate([1,3,2,1]) // true
+// hasDuplicate([1,5,-1,4]) // false
+
+const hasDuplicate = arr => new  Set(arr).size !==length
+
+// vowelCount('awesome') // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
+// vowelCount('Colt') // Map { 'o' => 1 }
+function vowel(char){
+    return  'aeiou'.includes(char);
+}
+
+function vowelCount(str){
+  let vowelMap = new Map();
+  for(let char of str){
+    let lC= char.toLowerCase()
+    if(vowel(lC)){
+      if(vowelMap.has(lC)){
+        vowelMap.set(lC, vowelMap.get(lC)+1);
+      } 
+      else {
+        vowelMap.set(lC,1);}
+      }
+    }
+    return vowelMap;
+}
