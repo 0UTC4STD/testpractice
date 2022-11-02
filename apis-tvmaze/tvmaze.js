33834d662bd2a6e1c31d6b7c3fdaba06155d3fe8
0UTC4STD <dylancourte@yahoo.com>
2022-11-02 12:31:15 -0400

    apis

---
+++ apis-tvmaze/tvmaze.js
@@ -0,0 +1,96 @@
+const MISSING_IMAGE_URL = "http://tinyurl.com/missing-tv";
+
+ async function searchShows(query) {
+  let response = await axios.get(
+    `http://api.tvmaze.com/search/shows?q=${query}`)
+    let shows = response.data.map(result => {
+     let show = result.show;
+    return {
+      id: show.id,
+      name: show.name,
+      summary: show.summary,
+      image: show.image ? show.image.medium : MISSING_IMAGE_URL,
+    };
+  });
+ return shows;}
+
+async function searchShows(query) {
+  return [
+    {
+      id: 1767,
+      name: "The Bletchley Circle",
+      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
+      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+    }
+  ]
+}
+
+function populateShows(shows) {
+  const $showsList = $("#shows-list");
+  $showsList.empty();
+
+  for (let show of shows) {
+    let $item = $(
+      `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
+         <div class="card" data-show-id="${show.id}">
+           <div class="card-body">
+             <h5 class="card-title">${show.name}</h5>
+             <p class="card-text">${show.summary}</p>
+           </div>
+         </div>
+       </div>
+      `);
+
+    $showsList.append($item);
+  }
+}
+
+$("#search-form").on("submit", async function handleSearch (evt) {
+  evt.preventDefault();
+
+  let query = $("#search-query").val();
+  if (!query) return;
+
+  $("#episodes-area").hide();
+
+  let shows = await searchShows(query);
+
+  populateShows(shows);
+});
+
+
+async function getEpisodes(id) {let response = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
+
+let episodes = response.data.map(episode => ({
+  id: episode.id,
+  name: episode.name,
+  season: episode.season,
+  number: episode.number,
+}));
+
+return episodes;
+}
+
+function populateEpisodes(episodes) {
+  const $episodesList = $("#episodes-list");
+  $episodesList.empty();
+    
+  for (let episode of episodes) {
+    let $item = $(
+      `<li>
+         ${episode.name}
+         (season ${episode.season}, episode ${episode.number})
+       </li>
+      `);
+
+    $episodesList.append($item);
+  }
+
+  $("#episodes-area").show();
+}
+
+$("#shows-list").on("click", ".get-episodes", async function handleEpisodeClick(evt) {
+  let showId = $(evt.target).closest(".Show").data("show-id");
+  let episodes = await getEpisodes(showId);
+  populateEpisodes(episodes);
+});
